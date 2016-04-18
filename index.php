5145c02d64b1b9e98d38a65df5bf2b6ef7270ed7
badaweya <badaweya.@yahoo.com>
2016-04-18 17:46:07 +0200

    adding index.php text

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>home</title>
+<link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap-rtl.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css" />
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+
+<body style="background-color:#FFF">
+    <nav style="background-color:#727dec">
+        <div class="container-fluid">
+           
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.htm">الرئيسية <span class="sr-only">(current)</span></a></li>
+                    <li><a href="">احجز لدى طبيبك</a></li>
+                    <li><a href="">اضف عيادتك</a></li>
+                     
+                </ul>
+<form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+         <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+                    
+            </div>
+        </div>
+        
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <img src="" width="100%" height="500" alt="لوجو">
+
+            </div>
+           <div class="col-md-9" style="direction:ltr;">
+                <div class="your-class">
+                    
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-3">
+                
+                <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="1">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c1" aria-expanded="false" aria-controls="c1">
+     اسنان
+        </a>
+      </h4>
+    </div>
+    <div id="c1" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="1">
+      <div class="panel-body">
+       <ul class="nav nav-pills nav-stacked">
+  <li><a href="temple1.html">د.احمد ابو العطا</a></li>
+  <li><a href="temple2.html">د.احمد صبحى</a></li>
+  <li><a href="temple3.html">د.ابراهيم عبد العزيز</a></li>
+  <li><a href="temple4.html">د.محمود عبدالله محمد مكى</a></li>
+  
+</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="2">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c2" aria-expanded="false" aria-controls="c2">
+         أنف وأذن وحنجرة 
+        </a>
+      </h4>
+    </div>
+    <div id="c2" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="2">
+      <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked">
+ <li><a href="museum1.html">د.احمد ابو الوفا الحجاجى<a></li>
+ <li><a href="museum2.html">د.عمرو ابو القاسم عبد الرحمن</a></li>
+</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="3">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c3" aria-expanded="false" aria-controls="c3">
+        النساء والتوليد 
+        </a>
+      </h4>
+    </div>
+    <div id="c3" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="3">
+      <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked">
+  <li><a href="hotel1.html">د.عماد خليل</a></li>
+  <li><a href="hotel2.html">د.رمضان عبد العزيز</a></li>
+  <li><a href="hotel4.html">د.علاء عبده حسنين</a></li>
+  
+  
+</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="4">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c4" aria-expanded="false" aria-controls="c4">
+  المخ والأعصاب 
+        </a>
+      </h4>
+    </div>
+    <div id="c4" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="4">
+      <div class="panel-body">
+       <ul class="nav nav-pills nav-stacked">
+  
+  <li><a href="">د.روماني عدلي يوسف</a></li>
+  <li><a href="">د.	طارق علي راجح  </a></li>
+  
+  
+</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="5">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c5" aria-expanded="false" aria-controls="c5">
+    الأطــفـــال 
+        </a>
+      </h4>
+    </div>
+    <div id="c5" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="5">
+      <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked">
+ 
+ <li><a href="">د.ميخائيل يونان نصرى</a></li>
+ <li><a href=""></a></li>
+ <li><a href=""></a></li>
+ 
+</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="6">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c6" aria-expanded="false" aria-controls="c6">
+          العيـــون
+        </a>
+      </h4>
+    </div>
+    <div id="c6" class="panel-collapse collapse" role="tabpanel" aria-labelledby="6">
+      <div class="panel-body">
+       <ul class="nav nav-pills nav-stacked">
+ 
+ <li><a href=""></a></li>
+ <li><a href=""></a></li>
+ <li><a href=""></a></li>
+ 
+ 
+</ul>
+      </div>
+    </div>
+  </div>
+  <div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="7">
+      <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#c7" aria-expanded="false" aria-controls="collapseThree">
+  العلاج الطبيعي
+        </a>
+      </h4>
+    </div>
+    <div id="c7" class="panel-collapse collapse" role="tabpanel" aria-labelledby="7">
+      <div class="panel-body">
+        <ul class="nav nav-pills nav-stacked">
+  <li><a href=""></a></li>
+  <li><a href=""></a></li>
+  <li><a href=""></a></li>
+  <li><a href=""></a></li>
+  
+</ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+               
+            </div>
+            <div class="col-md-9" style="direction:ltr;">
+                <div class="row">
+                    <div class="col-md-4">
+<div>
+   
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+
+ <hr>
+
+      <footer style="background-color:#727dec">
+        <p>&copy;2016</p>
+      </footer>
+
+    </div>
+
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js"></script>
+
+    <script>
+        $(document).ready(function(){
+        $('.your-class').slick({
+            autoplay: true,
+  autoplaySpeed: 2000,
+        });
+        });
+    </script>
+</body>
+</html>
